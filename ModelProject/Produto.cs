c7cd109e58b18d30acad8886f8aa79dc9d94c5ff
adsifro <ads_ifro_2022@outlook.com>
2023-03-27 21:57:14 -0400

    Adicionar arquivos de projeto.

---
+++ ModelProject/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelProject
+{
+    public class Produto
+    {
+        public Guid Id { get; set; }
+        public string Descricao { get; set; }
+        public double PrecoDeCusto { get; set; }
+        public double PrecoDeVenda { get; set; }
+        public double Estoque { get; set; }
+    }
+}
---
+++ PersistenceProject/Repository.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelProject;
+
+namespace PersistenceProject
+{
+    class Repository
+    {
+        private IList<Fornecedor> fornecedores
+            = new List<Fornecedor>();
+        private IList<Produto> produtos
+            = new List<Produto>();
+        private IList<NotaEntrada> notasEntrada
+            = new List<NotaEntrada>();
+
+        public Fornecedor InsertFornecedor
+            (Fornecedor fornecedor)
+        {
+            this.fornecedores.Add(fornecedor);
+            return fornecedor;
+        }
+
+        public void RemoveFornecedor
+            (Fornecedor fornecedor)
+        {
+            this.fornecedores.Remove(fornecedor);
+        }
+
+        public IList<Fornecedor> GetAllFornecedores()
+        {
+            return this.fornecedores;
+        }
+
+        public Fornecedor UpdateFornecedor
+            (Fornecedor fornecedor)
+        {
+            this.fornecedores[this.fornecedores.
+                IndexOf(fornecedor)] = fornecedor;
+            return fornecedor;
+        }
+
+        public Produto InsertProduto
+            (Produto produto)
+        {
+            this.produtos.Add(produto);
+            return produto;
+        }
+
+        public void RemoveProduto
+            (Produto produto)
+        {
+            this.produtos.Remove(produto);
+        }
+
+        public IList<Produto> GetAllProdutos()
+        {
+            return this.produtos;
+        }
+
+        public Produto UpdateProdutos
+            (Produto produto)
+        {
+            this.produtos[this.produtos.
+                IndexOf(produto)] = produto;
+            return produto;
+        }
+
+        public NotaEntrada InsertNotaEntrada
+            (NotaEntrada notaEntrada)
+        {
+            this.notasEntrada.Add(notaEntrada);
+            return notaEntrada;
+        }
+
+        public void RemoveNotaEntrada
+            (NotaEntrada notaEntrada)
+        {
+            this.notasEntrada.Remove(notaEntrada);
+        }
+
+        public IList<NotaEntrada> GetAllNotasEntrada()
+        {
+            return this.notasEntrada;
+        }
+
+        public NotaEntrada UpdateNotaEntrada
+            (NotaEntrada notaEntrada)
+        {
+            this.notasEntrada[this.notasEntrada.
+                IndexOf(notaEntrada)] = notaEntrada;
+            return notaEntrada;
+        }
+    }
+}
