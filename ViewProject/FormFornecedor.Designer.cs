88583b2a47a16f89f41b44741e08b037523ccd60
adsifro <ads_ifro_2022@outlook.com>
2023-04-10 19:44:38 -0400

    criado o método para seleção do dado no datagridview e adicionado o método clearcontrols

--- ViewProject/FormFornecedor.Designer.cs
+++ ViewProject/FormFornecedor.Designer.cs
@@ -63,80 +63,90 @@ namespace ViewProject
             this.tableLayoutPanel1.Controls.Add(this.btnGravar, 1, 3);
             this.tableLayoutPanel1.Controls.Add(this.btnCancelar, 0, 4);
             this.tableLayoutPanel1.Controls.Add(this.btnRemover, 1, 4);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(13, 9);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(20, 14);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 5;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.88733F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.11267F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(443, 162);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 46F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 54F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 58F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(664, 249);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // lblId
             // 
             this.lblId.AutoSize = true;
-            this.lblId.Location = new System.Drawing.Point(3, 0);
+            this.lblId.Location = new System.Drawing.Point(4, 0);
+            this.lblId.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblId.Name = "lblId";
-            this.lblId.Size = new System.Drawing.Size(16, 13);
+            this.lblId.Size = new System.Drawing.Size(23, 20);
             this.lblId.TabIndex = 0;
             this.lblId.Text = "Id";
             // 
             // txtId
             // 
             this.txtId.Enabled = false;
-            this.txtId.Location = new System.Drawing.Point(78, 3);
+            this.txtId.Location = new System.Drawing.Point(117, 5);
+            this.txtId.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtId.Name = "txtId";
-            this.txtId.Size = new System.Drawing.Size(362, 20);
+            this.txtId.Size = new System.Drawing.Size(540, 26);
             this.txtId.TabIndex = 1;
             // 
             // lblNome
             // 
             this.lblNome.AutoSize = true;
-            this.lblNome.Location = new System.Drawing.Point(3, 28);
+            this.lblNome.Location = new System.Drawing.Point(4, 43);
+            this.lblNome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNome.Name = "lblNome";
-            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.Size = new System.Drawing.Size(51, 20);
             this.lblNome.TabIndex = 2;
             this.lblNome.Text = "Nome";
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(78, 31);
+            this.txtNome.Location = new System.Drawing.Point(117, 48);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(362, 20);
+            this.txtNome.Size = new System.Drawing.Size(540, 26);
             this.txtNome.TabIndex = 3;
             // 
             // lblCnpj
             // 
             this.lblCnpj.AutoSize = true;
-            this.lblCnpj.Location = new System.Drawing.Point(3, 58);
+            this.lblCnpj.Location = new System.Drawing.Point(4, 90);
+            this.lblCnpj.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCnpj.Name = "lblCnpj";
-            this.lblCnpj.Size = new System.Drawing.Size(34, 13);
+            this.lblCnpj.Size = new System.Drawing.Size(49, 20);
             this.lblCnpj.TabIndex = 4;
             this.lblCnpj.Text = "CNPJ";
             // 
             // txtCnpj
             // 
-            this.txtCnpj.Location = new System.Drawing.Point(78, 61);
+            this.txtCnpj.Location = new System.Drawing.Point(117, 95);
+            this.txtCnpj.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtCnpj.Name = "txtCnpj";
-            this.txtCnpj.Size = new System.Drawing.Size(362, 20);
+            this.txtCnpj.Size = new System.Drawing.Size(540, 26);
             this.txtCnpj.TabIndex = 5;
             // 
             // btnNovo
             // 
-            this.btnNovo.Location = new System.Drawing.Point(3, 91);
+            this.btnNovo.Location = new System.Drawing.Point(4, 141);
+            this.btnNovo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnNovo.Name = "btnNovo";
-            this.btnNovo.Size = new System.Drawing.Size(69, 23);
+            this.btnNovo.Size = new System.Drawing.Size(104, 35);
             this.btnNovo.TabIndex = 6;
             this.btnNovo.Text = "Novo";
             this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
             // 
             // btnGravar
             // 
-            this.btnGravar.Location = new System.Drawing.Point(78, 91);
+            this.btnGravar.Location = new System.Drawing.Point(117, 141);
+            this.btnGravar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnGravar.Name = "btnGravar";
-            this.btnGravar.Size = new System.Drawing.Size(75, 23);
+            this.btnGravar.Size = new System.Drawing.Size(112, 35);
             this.btnGravar.TabIndex = 7;
             this.btnGravar.Text = "Gravar";
             this.btnGravar.UseVisualStyleBackColor = true;
@@ -144,18 +154,20 @@ namespace ViewProject
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(3, 126);
+            this.btnCancelar.Location = new System.Drawing.Point(4, 195);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(69, 23);
+            this.btnCancelar.Size = new System.Drawing.Size(104, 35);
             this.btnCancelar.TabIndex = 8;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
             // 
             // btnRemover
             // 
-            this.btnRemover.Location = new System.Drawing.Point(78, 126);
+            this.btnRemover.Location = new System.Drawing.Point(117, 195);
+            this.btnRemover.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnRemover.Name = "btnRemover";
-            this.btnRemover.Size = new System.Drawing.Size(75, 23);
+            this.btnRemover.Size = new System.Drawing.Size(112, 35);
             this.btnRemover.TabIndex = 9;
             this.btnRemover.Text = "Remover";
             this.btnRemover.UseVisualStyleBackColor = true;
@@ -165,20 +177,24 @@ namespace ViewProject
             this.dgvFornecedor.AllowUserToAddRows = false;
             this.dgvFornecedor.AllowUserToDeleteRows = false;
             this.dgvFornecedor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvFornecedor.Location = new System.Drawing.Point(12, 177);
+            this.dgvFornecedor.Location = new System.Drawing.Point(18, 272);
+            this.dgvFornecedor.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dgvFornecedor.Name = "dgvFornecedor";
             this.dgvFornecedor.ReadOnly = true;
+            this.dgvFornecedor.RowHeadersWidth = 62;
             this.dgvFornecedor.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvFornecedor.Size = new System.Drawing.Size(444, 261);
+            this.dgvFornecedor.Size = new System.Drawing.Size(666, 402);
             this.dgvFornecedor.TabIndex = 10;
+            this.dgvFornecedor.SelectionChanged += new System.EventHandler(this.dgvFornecedor_SelectionChanged);
             // 
             // FormFornecedor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(468, 450);
+            this.ClientSize = new System.Drawing.Size(702, 692);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.dgvFornecedor);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FormFornecedor";
             this.Text = "Manutenção de Dados do Fornecedor";
             this.tableLayoutPanel1.ResumeLayout(false);
--- ViewProject/FormFornecedor.cs
+++ ViewProject/FormFornecedor.cs
@@ -36,5 +36,31 @@ namespace ViewProject
             dgvFornecedor.DataSource = null;
             dgvFornecedor.DataSource = this.controller.GetAll();
         }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            txtId.Text = string.Empty;
+            txtNome.Text = string.Empty;
+            txtCnpj.Text = string.Empty;
+        }
+
+        private void dgvFornecedor_SelectionChanged(object sender, EventArgs e)
+        {
+            txtId.Text = dgvFornecedor.CurrentRow.Cells[0].
+                Value.ToString();
+            txtNome.Text = dgvFornecedor.CurrentRow.Cells[1].
+                Value.ToString();
+            txtCnpj.Text = dgvFornecedor.CurrentRow.Cells[2].
+                Value.ToString();
+        }
+
+        private void ClearControls()
+        {
+            dgvFornecedor.ClearSelection();
+            txtId.Text = string.Empty;
+            txtNome.Text = string.Empty;
+            txtCnpj.Text = string.Empty;
+            txtNome.Focus();
+        }
     }
 }
